2183bb7546bbe85c41497016c931ccf5358bb96d
itoalberti <itoalberti@gmail.com>
2025-12-20 18:34:41 -0300

    First commit

---
+++ AdditionCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿int n1 = 0;
+int n2  = 0;
+
+Console.WriteLine("Type in the first number");
+string userInput=Console.ReadLine();
+n1 =int.Parse(userInput);
+
+Console.WriteLine("Type in the second number");
+userInput=Console.ReadLine();
+n2=int.Parse(userInput);
+
+Console.WriteLine($"Sum {n1} + {n2} = {n1+n2}");
+
+
---
+++ MyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+	{
+		string myName;
+		myName = "Jobson";
+		Console.WriteLine(myName);
+		Console.ReadKey();
+    }
+}

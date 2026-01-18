2159da7ec349f2e88f6ba92defb42654ff4ff180
itoalberti <itoalberti@gmail.com>
2026-01-17 21:43:20 -0300

    Created NumberGuess

---
+++ NumberGuess/Program.cs
@@ -0,0 +1,11 @@
+﻿Random r = new Random();
+int randomNumber = r.Next(0, 11);
+Console.WriteLine("Guess a number from 0 to 10");
+int guess = int.Parse(Console.ReadLine());
+
+while (guess != randomNumber)
+{
+    Console.WriteLine("You guessed wrong! Guess again");
+    guess = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Now you got it right!");

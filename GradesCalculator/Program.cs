8cf24c2b3e53c47b59672be532390c374bd81f26
itoalberti <itoalberti@gmail.com>
2026-01-18 00:57:49 -0300

    Creation of GradesCalculator

---
+++ GradesCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = 0;
+double sum = 0;
+double grade = 0;
+
+do
+{
+    Console.WriteLine($"Enter the grade of student {i + 1}. Type -1 to exit.");
+    grade = double.Parse(Console.ReadLine());
+    if (grade != -1)
+    {
+        sum += grade;
+        i++;
+    }
+} while (grade != -1);
+
+double avg = Math.Round(sum / i, 2);
+Console.WriteLine($"The average grade of the class is {avg}");

b15e486207482b2a3d63850e8cfb4ae0f7f632cc
itoalberti <itoalberti@gmail.com>
2026-01-19 01:07:33 -0300

    Drafts (study purposes only)

---
+++ Drafts/Program.cs
@@ -0,0 +1,14 @@
+﻿void MyMethod(int a, int b)
+{
+    Console.WriteLine($"a+b= {a + b}");
+}
+
+MyMethod(5, 6);
+
+int number(int a, int b)
+{
+    return (a + b);
+}
+
+double c = 2.4156 * number(10, 20);
+Console.WriteLine(c);

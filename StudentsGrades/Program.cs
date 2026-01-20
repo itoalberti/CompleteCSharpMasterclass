936430178e6deb8305d891935da1b632e1892d18
itoalberti <itoalberti@gmail.com>
2026-01-19 22:52:32 -0300

    Creation of StudentsGrades

--- StudentsGrades/Program.cs
+++ StudentsGrades/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
 
-namespace DraftOldStyle;
+namespace StudentsGrades;
 
 class Program
 {
@@ -8,7 +9,6 @@ class Program
     {
         Console.WriteLine("Enter the number of students whose grades  you want to input:");
         int numberOfStudents = int.Parse(Console.ReadLine());
-        // string[] subjects = ["Arts", "Biology", "Chemistry", "English", "Mathematics"];
         var classGrades = new Dictionary<string, List<double>>()
         {
             { "Arts", new List<double>() },
@@ -17,16 +17,26 @@ class Program
             { "English", new List<double>() },
             { "Mathematics", new List<double>() },
         };
+        var avgGrades = new Dictionary<string, double>();
 
         foreach (var subject in classGrades.Keys)
         {
             Console.WriteLine($"----- STUDENTS' GRADES IN {subject.ToUpper()} ----- ");
+            double sum = 0;
             for (int i = 0; i < numberOfStudents; i++)
             {
                 Console.WriteLine($"Grade of student {i + 1}:");
                 double grade = double.Parse(Console.ReadLine());
                 classGrades[subject].Add(grade);
+                sum += grade;
             }
+            avgGrades[key: subject] = sum / numberOfStudents;
+        }
+        Console.WriteLine("GRADES SUMMARY");
+        foreach (var item in classGrades)
+        {
+            Console.WriteLine($"{item.Key}: {string.Join(" | ", item.Value)}");
+            Console.WriteLine($"Average grade of class in {item.Key}: {avgGrades[item.Key]:F2}");
         }
     }
 }

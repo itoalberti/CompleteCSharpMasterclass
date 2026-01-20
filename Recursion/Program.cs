7502b6a255747f182a06e2f494eed7dcd99e8027
itoalberti <itoalberti@gmail.com>
2026-01-19 23:20:08 -0300

    Creation of Recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,15 @@
+﻿static int Factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+Console.WriteLine(Factorial(5));
+Console.WriteLine(Factorial(6));
+Console.WriteLine(Factorial(15));

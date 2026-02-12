496c8d742925d038184a4f8ba09a5d765320e3b1
itoalberti <itoalberti@gmail.com>
2026-02-12 18:20:58 -0300

    Creation of TryCatch and Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿Dog myDog = new();
+myDog.Bark();
+myDog.MakeSound();
+myDog.Eat();
+
+class Animal
+{
+    public void Eat() => Console.WriteLine($"Eating...");
+
+    // to be able to override the base class method, it must be virtual
+    public virtual void MakeSound() => Console.WriteLine($"Generic animal makes a generic sound.");
+}
+
+class Dog : Animal
+{
+    public void Bark() => Console.WriteLine($"Barking...");
+
+    public override void MakeSound() => Console.WriteLine($"Generic sound now is a dog bark.");
+}
---
+++ TryCatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+try
+{
+    Console.WriteLine($"Type in the first number:");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Type in the second number:");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Result N1/N2 = {a / b}");
+}
+// catch (DivideByZeroException ex)
+// {
+//     Console.WriteLine($"IT'S IMPOSSIBLE TO DIVIDE BY ZERO!\n{ex}");
+// }
+catch (FormatException ex)
+{
+    Console.WriteLine($"YOU MUST TYPE NUMBERS TO PERFORM A CALCULATION\n{ex.Message}");
+}
+finally
+{
+    Console.WriteLine($"This line always executes, regardless of the errors");
+}

a7f953c6aec0b127651c830a3e7cf9fd96a7f0af
itoalberti <itoalberti@gmail.com>
2026-02-13 18:15:01 -0300

    created Inheritance

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -3,6 +3,9 @@ myDog.Bark();
 myDog.MakeSound();
 myDog.Eat();
 
+Student martin = new Student("Martin", 14, 9950, "Fifth grade");
+martin.DisplayPersonInfo();
+
 class Animal
 {
     public void Eat() => Console.WriteLine($"Eating...");
@@ -16,4 +19,37 @@ class Dog : Animal
     public void Bark() => Console.WriteLine($"Barking...");
 
     public override void MakeSound() => Console.WriteLine($"Generic sound now is a dog bark.");
+}
+
+public class Person
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+        Console.WriteLine($"Person constructor called");
+    }
+
+    public void DisplayPersonInfo()
+    {
+        Console.WriteLine($"========= PERSON DATA =========");
+        Console.Write($"Name: {Name} | Age: {Age}");
+    }
+}
+
+public class Student : Person
+{
+    public int StudentId { get; private set; }
+    public string Grade { get; private set; }
+
+    public Student(string name, int age, int studentId, string grade)
+        : base(name, age)
+    {
+        StudentId = studentId;
+        Grade = grade;
+        Console.WriteLine($"Student constructor called");
+    }
 }
